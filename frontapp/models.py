77e6a3c6e00944cd21fa88bd15951e91d2ab3211
fvon-nag <fvon-nag@student.42wolfsburg.de>
2024-05-31 11:22:08 +0200

    migration debugging

--- frontapp/models.py
+++ frontapp/models.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser ,Group, Permission
 from django.db import models
-from django.contrib.postgres.fields import JSONField
+from django.db.models import JSONField
+
 
 class CustomUser(AbstractUser):
     display_name = models.CharField(max_length=255, null=True, blank=True)
@@ -10,10 +11,33 @@ class CustomUser(AbstractUser):
     match_history = JSONField(default=list)  # array of custom structs for match history
     two_factor_auth_enabled = models.BooleanField(default=False)
     
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions'
+    )
     def __str__(self):
         return self.username
 
     class Meta:
         db_table = 'auth_user'
+
+
+class CustomUserGroup(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+class CustomUserPermission(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
 # You may need to add this in your settings.py
 # AUTH_USER_MODEL = 'yourapp.CustomUser'
