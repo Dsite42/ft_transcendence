a27da5470309f672a27548dbfc16f4789b3001cb
fvon-nag <fvon-nag@student.42wolfsburg.de>
2024-06-07 11:51:55 +0200

    fixed post requests not working after translation

--- frontend/urls.py
+++ frontend/urls.py
@@ -25,26 +25,42 @@ from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('accounts/', include('allauth.urls')),
+#     path('accounts/', include('allauth.socialaccount.urls')),
+#     path('', views.home, name='home'),
+#     path('i18n/', include('django.conf.urls.i18n')),
+#     path('play_pong/', views.play_pong, name='play_pong'),
+#     path('learn.html', learn_view, name='learn'),
+#     path('auth', auth),
+#     path('get_user_info', get_user_info),
+#     path('remove_all_otp_devices.html', views.remove_all_otp_devices, name='remove_otp'),
+#     path('verify_otp/', views.verify_otp, name='verify_otp'),
+#     path('send_friend_request/', views.send_friend_request, name='send_friend_request'),
+#     path('accept_friend_request/', views.accept_friend_request, name='accept_friend_request'),
+#     path('decline_friend_request/', views.decline_friend_request, name='decline_friend_request'),
+#     path('get_pending_friend_requests/', views.get_pending_friend_requests, name='get_pending_friend_requests'),
+# ]
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('accounts/', include('allauth.socialaccount.urls')),
-    path('', views.home, name='home'),
-    path('i18n/', include('django.conf.urls.i18n')),
-    path('play_pong/', views.play_pong, name='play_pong'),
-    path('learn.html', learn_view, name='learn'),
+    path('decline_friend_request/', views.decline_friend_request, name='decline_friend_request'),
+    path('change_info/', views.change_info, name='change_info'),
+    path('enable_otp/', views.enable_otp, name='enable_otp'),
+    path('login_with_otp/', views.login_with_otp, name='login_with_otp'),
     path('auth', auth),
-    path('get_user_info', get_user_info),
     path('remove_all_otp_devices.html', views.remove_all_otp_devices, name='remove_otp'),
     path('verify_otp/', views.verify_otp, name='verify_otp'),
-    path('change_info/', views.change_info, name='change_info'),
     path('send_friend_request/', views.send_friend_request, name='send_friend_request'),
     path('accept_friend_request/', views.accept_friend_request, name='accept_friend_request'),
     path('decline_friend_request/', views.decline_friend_request, name='decline_friend_request'),
-    path('get_pending_friend_requests/', views.get_pending_friend_requests, name='get_pending_friend_requests'),
 ]
 
-urlpatterns = i18n_patterns(
+urlpatterns += i18n_patterns(
+
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.socialaccount.urls')),
     path('', views.home, name='home'),
     path('i18n/', include('django.conf.urls.i18n')),
     path('tournament.html', views.tournament, name='tournament'),
@@ -57,6 +73,18 @@ urlpatterns = i18n_patterns(
     path('otp_login/', views.otp_login, name='otp_login'),
     path('login_with_otp/', views.login_with_otp, name='login_with_otp'),
     path('change_info_site.html', views.change_info_site, name='change_info_site'),
+    path('change_info/', views.change_info, name='change_info'),
+    path('', views.home, name='home'),
+    path('play_pong/', views.play_pong, name='play_pong'),
+    path('learn.html', learn_view, name='learn'),
+    path('auth', auth),
+    path('get_user_info', get_user_info),
+    path('remove_all_otp_devices.html', views.remove_all_otp_devices, name='remove_otp'),
+    path('verify_otp/', views.verify_otp, name='verify_otp'),
+    path('send_friend_request/', views.send_friend_request, name='send_friend_request'),
+    path('accept_friend_request/', views.accept_friend_request, name='accept_friend_request'),
+    path('decline_friend_request/', views.decline_friend_request, name='decline_friend_request'),
+    path('get_pending_friend_requests/', views.get_pending_friend_requests, name='get_pending_friend_requests'),
 )
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
