6ef517a8a20719c81b900d97848dca3524c923dd
Dsite42 <cgodecke@student.42wolfsburg.de>
2024-07-04 13:32:39 +0200

    code cleanup

--- static/js/tournament.js
+++ static/js/tournament.js
@@ -1,49 +1,3 @@
-function fetchTournamentData(tournamentId) {
-    fetch(`api/tournament/${tournamentId}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Tournament data:', data);
-            updatePageWithTournamentData(data);
-        })
-        .catch(error => console.error('Failed to fetch tournament data:', error));
-}
-
-function updatePageWithTournamentData(tournamentData) {
-    console.log('Updating page with tournament data:', tournamentData);
-    let matchesHtml = '';
-    if (tournamentData.matches && tournamentData.matches.length > 0) {
-        matchesHtml = '<div class="matches"><h2>Matches:</h2>';
-        tournamentData.matches.forEach(match => {
-            matchesHtml += `
-                <div class="match">
-                    <p>
-                        <span>Players: ${match.players.join(' vs ')}</span> |
-                        <span>Status: ${match.status}</span> |
-                        <span>Winner: ${match.winner || 'N/A'}</span>
-                    </p> 
-                </div>
-            `;
-        });
-        matchesHtml += '</div>';
-    }
-
-    document.getElementById('main-content').innerHTML = `
-    <div class="container mt-5 text-center">
-        <h1>Welcome to Tournament: <em>${tournamentData.name}</em></h1>
-        <p>Creator: ${tournamentData.creator}</p>
-        <p>Number of Players joined: ${tournamentData.players.length} / ${tournamentData.number_of_players}</p>
-        <p>Status: ${tournamentData.status}</p>
-        <p>Players: ${tournamentData.players.join(', ')}</p>
-        ${matchesHtml}
-    </div>
-    `;
-}
-
 function updatePageWithTournamentDataJson(tournamentData) {
     console.log('Updating page with tournament data:', tournamentData);
     let matchesHtml = '';
@@ -77,5 +31,4 @@ function updatePageWithTournamentDataJson(tournamentData) {
         ${tournamentWinnerHtml}
         </div>   
         `;
-        //<p>Players: ${tournamentData.display_names.join(', ')}</p>
 }
